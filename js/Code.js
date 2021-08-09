8e24a118a93cd13e9b1db32ca8e4d6a5f4e394b4
Aashe-Agarwal <74452200+Aashe-Agarwal@users.noreply.github.com>
2021-08-09 16:51:37 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,22 @@
+const accessCode1 = "RAIN";
+const accessCode2 = "BANK";
+const accessCode3 = "DARKNESS";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("What always goes up but never comes down?", 100,50)
+    
+
+    fill("white")
+    textSize(15)
+    text("I have branches, but no fruit, trunk or leaves. "+'\n'+" What am I?",700,150)
+
+
+    fill("white")
+    textSize(15)
+    text("The more you have this, the less you can see. What am I?",100,280)
+    
+
+}
